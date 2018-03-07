8def77efee5083cc9809fcfaa2f629d5568f66e6
hugo.adamietz1 <hugo.adamietz1@600900B211DE412>
2018-03-07 15:59:36 +1100

    commit all

---
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+
+    public class Moving : MonoBehaviour
+    {
+        public float acceleration = 5f;
+        public float rotationSpeed = 3f;
+        public float maxVelocity = 5f;
+
+        private Rigidbody2D rigid;
+        // Member variables
+        public float moveSpeed; //Declaring a variable
+        public float rotateSpeed;
+
+        private void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            //fMovement(Input.GetAxis("Vertical"));
+            //fRotation(-Input.GetAxis("Horizontal"));
+            LimitVelocity();
+        }
+
+        void fMovement(float yVal)
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime * yVal);
+        }
+
+        void fRotation(float xVal)
+        {
+            transform.rotation *= Quaternion.AngleAxis(rotateSpeed * xVal, Vector3.forward);
+        }
+        void LimitVelocity()
+        {
+            Vector3 vel = rigid.velocity;
+            if (vel.magnitude > maxVelocity)
+            {
+                vel = vel.normalized * maxVelocity;
+            }
+
+            rigid.velocity = vel;
+        }
+        public void Accelerate(Vector3 direction)
+        {
+            rigid.AddForce(direction * acceleration);            
+        }
+        public void RotateLeft()
+        {
+            rigid.rotation += rotationSpeed * Time.deltaTime;
+        }
+        void RotateRight()
+        {
+            rigid.rotation -= rotationSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+
+    public Transform[] spawnPoint;
+
+    public float shootRate = 0.5f; // in seconds
+    public float projectileSpeed = 5f;
+    public int damage = 20;
+    private int count = 0;
+    private float shootTimer = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        // Count up timer
+        shootTimer += Time.deltaTime; // in seconds (ms)
+
+        if (shootTimer >= shootRate && Input.GetKey(KeyCode.X))
+        {
+            // Loop through all spawn points
+            //shootv1
+            Shoot(spawnPoint[count]);
+            count++;
+            if (count == spawnPoint.Length)
+            {
+                count = 0;
+            }
+            //Shootv2
+            //for (int i = 0; i < spawnPoint.Length; i++)
+            //{
+            //    // Shoot a projectile
+            //    Shoot(spawnPoint[i]);
+            //}
+            // Reset timer
+            shootTimer = 0f;
+        }
+    }
+
+    // Logic for shooting a projectile
+    void Shoot(Transform point)
+    {
+        // Instantiate a projectile prefab
+        GameObject clone = Instantiate(projectilePrefab);
+        // Set position of clone to player
+        clone.transform.rotation = transform.rotation;
+        clone.transform.Rotate(90, 0, 0);
+        clone.transform.position = point.position;
+        // Add force to projectile physics
+            // Get Rigid Component
+            Rigidbody2D rigid = clone.GetComponent<Rigidbody2D>();
+            // Apply force
+            rigid.AddForce(transform.up * projectileSpeed, ForceMode2D.Impulse);
+    }
+}
